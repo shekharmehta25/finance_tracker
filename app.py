9f253415fd5754c8ce61fac375859093fa9657a7
Shekhar Mehta <shekharmehta25@gmail.com>
2023-04-06 21:49:52 -0400

    Dockerfile working

--- app.py
+++ app.py
@@ -88,5 +88,6 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.static_folder = 'templates'
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5002)
+
 
